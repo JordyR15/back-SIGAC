ade6650a8fec0ca69710af1640fb8c04c27f8b1c
Yerai <11yeraidavid@gmail.com>
2026-08-11 19:01:32 -0500

    Ligeros cambios en userdto y añadi materiaDto

---
+++ back/DTOs/MateriaDto.cs
@@ -0,0 +1,18 @@
+﻿namespace back.DTOs
+{
+    public class MateriaDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Codigo { get; set; }
+        public int DocenteResponsableId { get; set; }
+
+        // Opcional: si necesitas mostrar datos del docente
+        public string NombreDocenteResponsable { get; set; }
+
+        // Opcional: si necesitas incluir recursos o actividades
+        public List<RecursoDto> Recursos { get; set; }
+        public List<ActividadDto> Actividades { get; set; }
+    }
+}
--- back/DTOs/UserDto.cs
+++ back/DTOs/UserDto.cs
@@ -2,9 +2,12 @@ namespace back.DTOs
 {
     public class UserDto
     {
-        public int Id { get; set; } // Añadido
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Token { get; set; }
         public string Rol { get; set; }
+        public string Nombre { get; set; }    // Agregar
+        public string Apellido { get; set; }  // Agregar
+        public string Correo { get; set; }
     }
 }
