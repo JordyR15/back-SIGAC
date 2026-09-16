559ada421cbed97fd1980d1023ef469dec82b43b
Yerai <11yeraidavid@gmail.com>
2026-09-15 19:30:24 -0500

    Correccion del apartado de clases y materias

---
+++ start_server.ps1
@@ -0,0 +1,3 @@
+$env:ASPNETCORE_ENVIRONMENT = "Development"
+Set-Location "C:\Users\11yer\IdeaProjects\back-SIGAC"
+& "C:\Program Files\dotnet\dotnet.exe" run --project "C:\Users\11yer\IdeaProjects\back-SIGAC\back\back.csproj" --urls "http://0.0.0.0:5001"
